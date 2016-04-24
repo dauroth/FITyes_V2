b2acef17cdd33cd771a8045e79c7853f476db373
dauroth <dauroth@gmail.com>
2016-04-24 22:12:36 +0200

    Initial commit

---
+++ app/src/main/java/com/mattlab/gym/fityes_v2/CalendarFragment.java
@@ -0,0 +1,45 @@
+package com.mattlab.gym.fityes_v2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarFragment extends Fragment {
+
+    private View parentView;
+    private ListView listView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        parentView = inflater.inflate(R.layout.calendar, container, false);
+        listView   = (ListView) parentView.findViewById(R.id.listView);
+        initView();
+        return parentView;
+    }
+
+    private void initView(){
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                getCalendarData());
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), "Listaelem lenyomva", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private ArrayList<String> getCalendarData(){
+        ArrayList<String> calendarList = new ArrayList<String>();
+        calendarList.add("Karizom edzés");
+        return calendarList;
+    }
+}
