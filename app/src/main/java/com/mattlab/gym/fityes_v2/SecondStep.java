9d9c07316d48583fbb2e1e7bac0e683cc24d9bee
dauroth <dauroth@gmail.com>
2016-04-25 10:14:27 +0200

    ThirdStep

--- app/src/main/java/com/mattlab/gym/fityes_v2/SecondStep.java
+++ app/src/main/java/com/mattlab/gym/fityes_v2/SecondStep.java
@@ -3,6 +3,8 @@ package com.mattlab.gym.fityes_v2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondStep extends AppCompatActivity {
 
@@ -11,6 +13,20 @@ public class SecondStep extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondstep); //test
 
+        Button btn = (Button) findViewById(R.id.btn_save_next);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NextStep(v);
+            }
+        });
+    }
+
+
+    public void NextStep(View v) {
+        Intent myIntent = new Intent(SecondStep.this, ThirdStep.class);
+        myIntent.putExtra("key", "2"); //Optional parameters
+        SecondStep.this.startActivity(myIntent);
     }
 }
